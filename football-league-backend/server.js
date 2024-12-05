f343a5b8299adf91474a69e44a11dadbaeb672e2
Drew <freeski0706@outlook.com>
2024-12-05 13:55:02 -0500

    env to root

--- football-league-backend/server.js
+++ football-league-backend/server.js
@@ -24,7 +24,7 @@ app.use(express.json());
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect("mongodb+srv://Free:drew@staywok.mhqbt.mongodb.net/", { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error(err));
 
