aec476c2b563fdedc8b7e1712bc0296f3ee9e12c
Drew <freeski0706@outlook.com>
2024-12-05 14:13:05 -0500

    update api

--- football-league-backend/server.js
+++ football-league-backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-
+const path = require('path');
 const cors = require('cors');
 const User = require('./models/User');
 const Team = require('./models/Team');
